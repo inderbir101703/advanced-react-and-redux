a72156e567c22301d8f4dc70f33d6f53561d24be
Inderbir Singh <indsingh11@WKMZTC5D52AB.global.publicisgroupe.net>
2024-02-07 23:17:09 +0530

    reducer

---
+++ src/reducers/comment.js
@@ -0,0 +1,12 @@
+const intialStage={
+    comments:'check 123'
+}
+function commentsReducer(state=intialStage,action){
+    switch(action.type)
+    {
+       default:
+        return state 
+    }
+
+}
+export default commentsReducer
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import commentsReducer from "./comment";
+export default combineReducers({
+comments:commentsReducer
+})
